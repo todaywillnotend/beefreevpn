c477dfb9d2f44dcf0ad19438e75a757bf02848c7
Eloy <todaywillnotend@gmail.com>
2025-06-01 22:26:58 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9a1973ed98d608a16d2f.js"
+    "url": "webpack-runtime-a1092a0428f1fd314735.js"
   },
   {
     "url": "framework-cdce76d1477a651fcfdd.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9ec722ac205f4754f9b57dcf99d8ab6d"
+    "revision": "d8a7ad53ee79178e2d1df95dae8afac2"
   },
   {
     "url": "manifest.webmanifest",
